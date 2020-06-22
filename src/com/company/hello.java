506430912278e955303305d3344f81eade86ff55
rohankamboj65 <rohankamboj65@gmail.com>
2020-06-22 12:03:36 +0530

    3rd

--- src/com/company/hello.java
+++ src/com/company/hello.java
@@ -2,7 +2,7 @@ package com.company;
 import java.util.*;
 public class hello {
   public static void main(String args[]){
-      int a=11;
+      int a=12;
     //hello rohan
       System.out.print(a);
   }
